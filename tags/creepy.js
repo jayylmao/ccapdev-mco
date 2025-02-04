306c57c77505c9e2cf346433beecb77c76f10ccc
jayylmao <73204320+jayylmao@users.noreply.github.com>
2025-02-04 14:37:43 +0800

    feat: add tag pages

---
+++ tags/creepy.js
@@ -0,0 +1,10 @@
+import { displayPost } from "../render/render-post.js";
+import { users } from "../test-data.js";
+
+users.forEach((user) => {
+    user.userPosts.forEach((post) => {
+        if (post.getTags().includes("creepy")) {
+            displayPost(post, "main-content");
+        }
+    });
+});
