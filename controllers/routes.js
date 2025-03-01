35c1f8c10e566631d0f41641fb78df4b3dfa5d6b
jayylmao <73204320+jayylmao@users.noreply.github.com>
2025-03-01 20:17:49 +0800

    feat: add mongodb export

--- controllers/routes.js
+++ controllers/routes.js
@@ -2,12 +2,46 @@ const { MongoClient } = require('mongodb') ;
 const databaseURL = 'mongodb://127.0.0.1:27017/';
 const mongoClient = new MongoClient (databaseURL);
 
+const databaseName = 'rabble';
+const postCollection = 'posts';
+const userCollection = 'user';
+const commentCollection = 'comments';
+
+/**
+ * initialConnection() starts a connection to the Mongo database and initializes
+ * the collections if the database does not contain them.
+ */
+async function initialConnection() {
+	let connection = await mongoClient.connect();
+	console.log("Creating collections...");
+	const database = mongoClient.db(databaseName);
+
+	database.createCollection(postCollection);
+	database.createCollection(userCollection);
+	database.createCollection(commentCollection);
+}
+
+/**
+ * add() Adds the routes to the Express server instance.
+ * @param {Express} server Express server instance to add routes to.
+ */
 function add(server) {
+	initialConnection();
+
 	// render main page.
 	server.get('/', function(req, resp) {
 		resp.render('main', {
 			layout: 'index',
 			title: 'rabble'
+		});
+	});
+
+	// render post page.
+	server.get('/post', function(req, resp) {
+		resp.render('post', {
+			layout: 'post',
+			title: 'rabble - post',
+			post: post
 		});
 	});
 }
