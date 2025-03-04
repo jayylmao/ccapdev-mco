10061c8fa634d83bbc53964cf802173ac30caa7b
jayylmao <73204320+jayylmao@users.noreply.github.com>
2025-03-04 15:09:58 +0800

    fix: formatting for user and date on post viewer page

--- controllers/routes.js
+++ controllers/routes.js
@@ -57,8 +57,6 @@ function add(server) {
         ]);
         let vals = await cursor.toArray();
 
-        console.log(vals);
-
         resp.render('main', {
             layout: 'index_layout',
             pageTitle: 'rabble',
@@ -70,20 +68,53 @@ function add(server) {
     server.get('/post/:id([0-9a-fA-F]{24})', async function(req, resp) {
         const dbo = mongoClient.db(databaseName);
         const posts = dbo.collection(postCollection);
+        const users = dbo.collection(userCollection);
 
         // find post with id specified in url.
-        let post = await posts.findOne({
-            _id: ObjectId.createFromHexString(req.params.id)
-        });
+        let cursor = await posts.aggregate([
+            { $match: { _id: ObjectId.createFromHexString(req.params.id) } },
+            { $project: {
+                "_id": 1,
+                "tags": 1,
+                "postCreator": 1,
+                "datePosted": { $dateToString: { date: "$datePosted", format: "%B %d, %Y", onNull: "Unknown date" }},	// convert date from object to proper string format.
+                "title": 1,
+                "content": 1,
+                "votes": 1,
+                "postCreatorObj.username": 1,
+                "postCreatorObj.profileImg": 1
+            }}
+        ]).toArray();
 
+        let post = cursor[0];
+        console.log(post);
+        
         if (!post) {
             return resp.status(404).send('Post not found.');
         }
 
+        // find user who made post.
+        cursor = posts.aggregate([
+            { $match: { _id: ObjectId.createFromHexString(req.params.id) } },   // get post id from request id.
+            { $lookup: {
+                from: "users",                                                  // search in users collection.
+                localField: "postCreator",                                      // search query is post creator id from post.
+                foreignField: "_id",                                            // match search query to _id field in users collection.
+                as: "postCreatorObj"                                            // save result to postCreatorObj.
+            }},
+            { $unwind: "$postCreatorObj" },                                     // flatten results as array.
+            { $project: {
+                username: "$postCreatorObj.username"                            // retrieve only username from results.
+            }}
+        ]);
+
+        let username = await cursor.toArray();
+
         resp.render('post', {
             layout: 'post_viewer_layout',
             pageTitle: 'rabble - ' + post.title,
-            post: post
+            post: post,
+            username: username[0].username
         });
 
     });
@@ -115,8 +146,6 @@ function add(server) {
         ]);
 
         let postsArray = cursor.toArray();
-
-        console.log(user);
 
         resp.render('profile_page', {
             layout: 'user_profile_layout',
