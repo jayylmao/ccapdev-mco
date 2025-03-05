8a02599a7125223f8d8f68da969176ab91e2ee08
jayy <73204320+jayylmao@users.noreply.github.com>
2025-03-05 19:32:36 +0800

    feat: main page displayed

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -1,14 +1,13 @@
-const User = require('../models/user-model.js');
 const Post = require('../models/post-model.js');
 
 const renderMainPage = async (req, res) => {
     try {
         // get 10 most voted posts.
-        let posts = Post.aggregate([
+        let posts = await Post.aggregate([
             { $sort: { votes: -1 } }, 			// sort posts by descending.
             { $limit: 10 }, 					// only get 10.
             { $lookup: {
-                from: User          ,			// get post creator from user collection.
+                from: "users",			// get post creator from user collection.
                 localField: "postCreator",		// search query is post creator id in post collection.
                 foreignField: "_id",			// search in _id field in user collection.
                 as: "postCreatorObj"			// save results to postCreatorObj field in post.
@@ -18,14 +17,14 @@ const renderMainPage = async (req, res) => {
                 "_id": 1,
                 "tags": 1,
                 "postCreator": 1,
-                "datePosted": { $dateToString: { date: "$datePosted", format: "%B %d, %Y", onNull: "Unknown date" }},	// convert date from object to proper string format.
+                "datePosted": 1,
                 "title": 1,
                 "content": 1,
                 "votes": 1,
                 "postCreatorObj.username": 1,
                 "postCreatorObj.profileImg": 1
             }}
-        ]);
+        ]).exec();
 
         console.log(posts);
 
