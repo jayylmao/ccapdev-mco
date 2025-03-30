a04a8737ac2a1828becd32afb073c9e74868822a
naysa <164517133+nadaqui@users.noreply.github.com>
2025-03-30 21:14:33 +0800

    added password encryption

--- controllers/account-controller.js
+++ controllers/account-controller.js
@@ -1,4 +1,5 @@
 const user = require('../models/user-model.js');
+const bcrypt = require('bcrypt');
 
 const renderLoginPage = async (req, res) => {
     res.render('login',{
@@ -15,19 +16,22 @@ const renderRegisterPage = async (req, res) => {
 }
 
 const registerData = async (req,res) => {
+    let pass1 = req.body.password1;
+    let pass2 = req.body.password2;
+
+    // encrypt the first pass
+    let hash = await bcrypt.hash(pass1, 10)
+
     const data = {
         username : req.body.username,
         fName : req.body.firstname,
         lName : req.body.lastname,
-        password : req.body.password1,
+        password : hash,
         description : 'No description yet',
         backgroundImg : 'https://png.pngtree.com/background/20230616/original/pngtree-faceted-abstract-background-in-3d-with-shimmering-iridescent-metallic-texture-of-picture-image_3653595.jpg',
         profileImg : 'https://i.pinimg.com/1200x/98/1d/6b/981d6b2e0ccb5e968a0618c8d47671da.jpg'
     }
 
-    let pass1 = req.body.password1;
-    let pass2 = req.body.password2;
-    
     // check if both passwords are same 
     if (pass1 != pass2) {
         res.redirect('/account/error');
@@ -40,9 +44,11 @@ const registerData = async (req,res) => {
 const loginData = async (req,res) => {
     try{
         const check = await user.findOne({username:req.body.username});
+        let inputPass = req.body.password;
+        const isMatch = await bcrypt.compare(inputPass, check.password);
 
         // if password matches that of the user
-        if (check.password === req.body.password) {
+        if (isMatch) {
             res.redirect('/home');
         } else {
             res.redirect('/account/error');
