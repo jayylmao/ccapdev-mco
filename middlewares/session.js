415be696b6bc7d551c4b9d76fdd5e78ecb432121
jayy <73204320+jayylmao@users.noreply.github.com>
2025-03-31 11:40:31 +0800

    feat: add mongo session storage

--- middlewares/session.js
+++ middlewares/session.js
@@ -1,7 +1,22 @@
-require ('dotenv').config();
+const dotenv = require('dotenv');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('connected to mongodb.'))
+  .catch(err => console.error('could not connect to mongodb: ', err));
 
 const sessionMiddleware = session({
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI,
+        collectionName: 'sessions',
+        ttl: 1000 * 60 * 60 * 24 * 30, // 30 days expiration.
+    }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
