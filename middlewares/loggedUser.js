c52e7e0545690801e8e37f4f0dbab5a42b83e88b
jayy <73204320+jayylmao@users.noreply.github.com>
2025-04-03 03:10:23 +0800

    fix: login does not work

--- middlewares/loggedUser.js
+++ middlewares/loggedUser.js
@@ -1,5 +1,6 @@
 // allow global access to the currently logged-in user.
 const loggedUser = async (req, res, next) => {
+    console.log(res.locals.user);
 	res.locals.user = req.session.user || null;
 	next();
 }
