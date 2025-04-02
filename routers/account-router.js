34fe3be8a2542cc692d350f70266d8d247706127
naysa <164517133+nadaqui@users.noreply.github.com>
2025-04-02 22:20:54 +0800

    deleting accounts

--- routers/account-router.js
+++ routers/account-router.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {renderLoginPage, renderRegisterPage, registerData, loginData, logoutData, renderErrorPage} = require('../controllers/account-controller.js');
+const {renderLoginPage, renderRegisterPage, registerData, loginData, logoutData, deleteUser, renderErrorPage} = require('../controllers/account-controller.js');
 
 router.route('/login').get(renderLoginPage);
 router.route('/login').post(loginData);
 
 router.route('/logout').get(logoutData);
+router.route('/deleteUser').get(deleteUser);
 
 router.route('/register').get(renderRegisterPage);
 router.route('/register').post(registerData);
