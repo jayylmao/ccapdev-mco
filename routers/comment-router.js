e53edfc2b9d8327ee54bf0bf2506050ca412f379
MarkCelemen <edrianmark923@gmail.com>
2025-04-02 18:05:18 +0800

    feat: added delete comment functionality (actually just flagged delete)

--- routers/comment-router.js
+++ routers/comment-router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { renderCommentEditorPage, updateComment } = require('../controllers/comment-controller.js');
+const { renderCommentEditorPage, editComment, flagComment } = require('../controllers/comment-controller.js');
 
+// GET request to render the edit comment page
 router.get('/edit/:id', renderCommentEditorPage);
-router.post('/edit/:id', updateComment);
+router.post('/edit/:id', editComment);
+router.post('/flag/:id', flagComment);
 
 module.exports = router;
